571fe5005d811a8716f4c54940deb4a57564383c
Alexandre Lino <149592058+alexlinofer@users.noreply.github.com>
2024-10-28 16:57:02 -0300

    Added CarEditor Buttons
    - Adicionado mais métodos e funções ao script CarEditor
    - Agora CarEditor instancia um prefab na cena ao clicar no botão

--- Assets/Scripts/TestScript/Editor/CarEditor.cs
+++ Assets/Scripts/TestScript/Editor/CarEditor.cs
@@ -11,18 +11,29 @@ public class CarEditor : Editor
         //base.OnInspectorGUI();
         Car myTarget = (Car)target;
 
+        myTarget.carPrefab = (GameObject)EditorGUILayout.ObjectField(myTarget.carPrefab, typeof(GameObject), true);
+
+        
         myTarget.speed = EditorGUILayout.IntField("My Speed is", myTarget.speed);
         myTarget.gear = EditorGUILayout.IntField("My Gear is", myTarget.gear);
 
         EditorGUILayout.LabelField("My Total Speed is", myTarget.TotalSpeed.ToString());
 
         EditorGUILayout.HelpBox("Calculate the total speed", MessageType.Info);
-        
 
+        GUI.color = Color.red;
         if(myTarget.TotalSpeed > 200)
         {
             EditorGUILayout.HelpBox("Oh no! Too fast!", MessageType.Error);
         }
+
+        GUI.color = Color.yellow;
+
+        if(GUILayout.Button("Create Car"))
+        {
+            myTarget.CreateCar();
+        }
+
     }
 
 
